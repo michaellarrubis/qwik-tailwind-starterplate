5a3d48ccb1b57e4476d25cc69c0808f5f797ad87
michaellarrubis <michaellarrubis@gmail.com>
2023-04-27 18:06:06 +0800

    Update: Reverts changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <p class="font-bold text-2xl">Takaon!</p>
+    <p class="font-bold text-2xl">Welcome! This is a starter template for Qwik + Tailwind</p>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Takaon',
+  title: 'Boilerplate: Qwik & Tailwind ',
   meta: [
     {
       name: 'description',
